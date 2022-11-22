61be154dc15c9f11c2fefa0994f5e5b2005bd873
powcoder <powcoder@163.com>
2022-11-22 11:57:37 +0800

    COMP251 HW4 templates F2019

---
+++ Multiply.java
@@ -0,0 +1,68 @@
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+import java.util.*;
+import java.io.*;
+
+public class Multiply{
+
+    private static int randomInt(int size) {
+        Random rand = new Random();
+        int maxval = (1 << size) - 1;
+        return rand.nextInt(maxval + 1);
+    }
+    
+    public static int[] naive(int size, int x, int y) {
+
+        // YOUR CODE GOES HERE  (Note: Change return statement)
+        
+        return null;
+        
+    }
+
+    public static int[] karatsuba(int size, int x, int y) {
+        
+        // YOUR CODE GOES HERE  (Note: Change return statement)
+        
+        return null;
+        
+    }
+    
+    public static void main(String[] args){
+
+        try{
+            int maxRound = 20;
+            int maxIntBitSize = 16;
+            for (int size=1; size<=maxIntBitSize; size++) {
+                int sumOpNaive = 0;
+                int sumOpKaratsuba = 0;
+                for (int round=0; round<maxRound; round++) {
+                    int x = randomInt(size);
+                    int y = randomInt(size);
+                    int[] resNaive = naive(size,x,y);
+                    int[] resKaratsuba = karatsuba(size,x,y);
+            
+                    if (resNaive[0] != resKaratsuba[0]) {
+                        throw new Exception("Return values do not match! (x=" + x + "; y=" + y + "; Naive=" + resNaive[0] + "; Karatsuba=" + resKaratsuba[0] + ")");
+                    }
+                    
+                    if (resNaive[0] != (x*y)) {
+                        int myproduct = x*y;
+                        throw new Exception("Evaluation is wrong! (x=" + x + "; y=" + y + "; Your result=" + resNaive[0] + "; True value=" + myproduct + ")");
+                    }
+                    
+                    sumOpNaive += resNaive[1];
+                    sumOpKaratsuba += resKaratsuba[1];
+                }
+                int avgOpNaive = sumOpNaive / maxRound;
+                int avgOpKaratsuba = sumOpKaratsuba / maxRound;
+                System.out.println(size + "," + avgOpNaive + "," + avgOpKaratsuba);
+            }
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+
+   } 
+}
